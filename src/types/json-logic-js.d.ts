36de1d35f82e4ab550a608ed49a7f8ad638214ee
Benjamin-Yu-Sheng-Chang <benji.chang@mail.utoronto.ca>
2025-04-01 14:00:04 -0400

    minor change

--- src/types/json-logic-js.d.ts
+++ src/types/json-logic-js.d.ts
@@ -5,6 +5,7 @@ declare module 'json-logic-js' {
     rm_operation: (name: string) => void;
     is_logic: (rule: any) => boolean;
     truthy: (value: any) => boolean;
+    rule_like: (rule: any, pattern: any) => boolean;
   }
 
   const jsonLogic: JsonLogic;
